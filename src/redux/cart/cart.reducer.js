cea83d3423afc5c942cf6375c4f465bf3a3ef0c1
PreciousNiphemi <preciousoluwanifemi1999@gmail.com>
2020-10-19 14:04:16 -1200

    added addItem and removeItem functionality to checkout

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,7 @@
+
 import {CartActionTypes} from './cart.types';
 
-import {addItemToCart} from './cart.utils';
+import {addItemToCart, removeItemFromCart} from './cart.utils';
 
 
 const INITIAL_STATE ={
@@ -20,7 +21,20 @@ switch(action.type){
             ...state,
             cartItems:addItemToCart(state.cartItems, action.payload)
         } 
+    
+    case CartActionTypes.REMOVE_ITEM:
+        return{
+            ...state,
+            cartItems: removeItemFromCart(state.cartItems, action.payload)
+        }
 
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+        return{
+            ...state,
+            cartItems: state.cartItems.filter(
+                cartItem => cartItem.id !== action.payload.id
+                )
+        }
         default:
             return state;
 }
